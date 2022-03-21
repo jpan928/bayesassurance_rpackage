f3ea657763e12f49e02266fca03844746a820a85
jpan928 <jpan1@ucla.edu>
2022-03-21 11:29:22 -0700

    Add R files

---
+++ R/bayes_adcock.R
@@ -0,0 +1,84 @@
+#' Bayesian Assurance Computation in the Precision-Based Setting
+#'
+#' Returns the Bayesian assurance of observing that the
+#' absolute difference between the true underlying
+#' population parameter and the sample estimate falls within a margin
+#' of error no greater than a fixed precision level, \eqn{d}.
+#' @importFrom ggplot2 ggplot aes
+#' @importFrom rlang .data
+#' @importFrom pbapply pbsapply
+#' @param n sample size (either vector or scalar).
+#' @param d fixed precision level
+#' @param mu_beta_a analysis stage mean
+#' @param mu_beta_d design stage mean
+#' @param n_a sample size at analysis stage. Also quantifies the amount of prior information we have for
+#' parameter \eqn{\mu}.
+#' @param n_d sample size at design stage. Also quantifies the amount of prior information we have
+#' for where the data is being generated from.
+#' @param sig_sq Known variance \eqn{\sigma^2}.
+#' @param alpha significance level
+#' @param mc_iter number of MC samples evaluated under the analysis objective
+#' @return approximate Bayesian assurance under precision-based conditions
+#' @examples
+#' n <- seq(20, 145, 5)
+#' out <- bayes_adcock(n = n, d = 0.20, mu_beta_a = 0.64, mu_beta_d = 0.9,
+#'                       n_a = 20, n_d = 10, sig_sq = 0.265,
+#'                       alpha = 0.05, mc_iter = 10000)
+#' head(out$assurance_table)
+#' out$assurance_plot
+#' @export
+#'
+
+bayes_adcock <- function(n, d, mu_beta_a, mu_beta_d, n_a, n_d, sig_sq, alpha, mc_iter = 1000){
+
+  count <- 0
+
+  MC_sample <- function(n = n){
+  for(i in 1:mc_iter){
+    # Design Stage Begins
+    var_d <- sig_sq * ((n_d + n) / (n * n_d))
+    xbar <- rnorm(n=1, mean = mu_beta_d, sd = sqrt(var_d))
+    # Design Stage Ends
+
+    # Analysis Stage Begins
+
+    # posterior mean
+    lambda <- ((n_a * mu_beta_a) + (n * xbar)) / (n_a + n)
+
+    phi_1 <- (sqrt(n_a + n) / sqrt(sig_sq)) * (xbar + d - lambda)
+    phi_2 <- (sqrt(n_a + n) / sqrt(sig_sq)) * (xbar - d - lambda)
+
+    # Analysis Stage Objective
+    Zi <- ifelse(pnorm(phi_1) - pnorm(phi_2) >= 1 - alpha, 1, 0)
+
+    count <- ifelse(Zi == 1, count <- count + 1, count <- count)
+    # Analysis Stage Ends
+  }
+
+  assurance <- count / mc_iter
+  return(assurance)
+  }
+
+  # checks the analysis objective for each n
+  assurance <- pbapply::pbsapply(n, function(i) MC_sample(n=i))
+
+  # Assurance table
+  assur_tab <- as.data.frame(cbind(n, assurance))
+  colnames(assur_tab) <- c("n", "Assurance")
+  assur_tab <- structure(assur_tab, class = "data.frame")
+
+  # creates ggplot for vector entries of n
+  if(length(n) > 1){
+    assur_plot <- ggplot2::ggplot(assur_tab, alpha = 0.5, aes(x = .data$n, y = .data$Assurance)) +
+      ggplot2::geom_point(aes(x = .data$n, y = .data$Assurance), lwd = 1.2) + ggplot2::ggtitle("Assurance Values") +
+      ggplot2::xlab("Sample Size n") + ggplot2::ylab("Assurance")
+    assur_plot <- structure(assur_plot, class = "ggplot")
+  }
+
+  if(length(n) > 1){
+    return(list(assurance_table = assur_tab, assurance_plot = assur_plot, mc_samples = mc_iter))
+  }else{
+    return(list(assur_val = paste0("Assurance: ", round(assurance, 3)), mc_samples = mc_iter))
+  }
+
+}
---
+++ R/bayes_sim_betabinomial.R
@@ -0,0 +1,152 @@
+#' Bayesian Assurance Computation in the Beta-Binomial Setting
+#'
+#' Returns the Bayesian assurance corresponding to a hypothesis test for
+#' difference in two independent proportions.
+#' @importFrom ggplot2 ggplot aes
+#' @importFrom rlang .data
+#' @importFrom pbapply pbsapply
+#' @param n1 sample size of first group
+#' @param n2 sample size of second group
+#' @param p1 proportion of successes in first group. Takes on a NULL (default) assignment
+#' if unknown.
+#' @param p2 proportion of successes in second group. Takes on a NULL (default) assignment
+#' if unknown.
+#' @param alpha_1,beta_1 shape parameters for the distribution of p1 if p1 is not known:
+#' \eqn{p1 ~ Beta(\alpha_1, \beta_1)}
+#' @param alpha_2,beta_2 shape parameters for the distribution of p2 if p2 is not known:
+#' \eqn{p2 ~ Beta(\alpha_2, \beta_2)}
+#' @param sig_level significance level
+#' @param alt a character string specifying the alternative hypothesis, must be one of
+#' "two.sided" (default), "greater" or "less".
+#' @param mc_iter number of MC samples evaluated under the analysis objective
+#' @returns approximate Bayesian assurance of independent two-sample proportion test
+#' @examples
+#'
+#' # Simulation that overlays frequentist power with Bayesian assurance
+#' # for independent two-sample proportion test.
+#'
+#' library(ggplot2)
+#'
+#' #########################################################
+#' # alpha1 = 0.5, beta1 = 0.5, alpha2 = 0.5, beta2 = 0.5 ##
+#' #########################################################
+#' n <- seq(40, 1000, 10)
+#'
+#' power_vals <- sapply(n, function(i) bayesassurance::propdiffCI_classic(i, p1 = 0.25, p2 = 0.2, alpha_1 = NULL,
+#' beta_1 = NULL, alpha_2 = NULL, beta_2 = NULL, sig_level = 0.05))
+#'
+#' df1 <- as.data.frame(cbind(n, power_vals))
+#'
+#' assur_vals <- sapply(n, function(i) bayesassurance::bayes_sim_betabin(n1 = i, n2 = i, p1 = 0.25, p2 = 0.2, alpha_1 = 0.5, beta_1 = 0.5,
+#'                                         alpha_2 = 0.5, beta_2 = 0.5, sig_level = 0.05, alt = "two.sided"))
+#'
+#' df2 <- as.data.frame(cbind(n, assur_vals))
+#'
+#' p1 <- ggplot(df1, alpha = 0.5, aes(x = n, y = power_vals, color="Frequentist Power"))
+#' p1 <- p1 + geom_line(alpha = 0.5, aes(x = n, y = power_vals, color="Frequentist Power"), lwd = 1.2)
+#' p2 <- p1 + geom_point(data = df2, alpha = 0.5, aes(x = n, y = assur_vals, color = "Bayesian Assurance"),
+#' lwd = 1.2) + ylab("Power/Assurance") + xlab(~ paste("Sample Size n = ", "n"[1], " = ", "n"[2])) +
+#' ggtitle("Power/Assurance Curves of Difference in Proportions")
+#' p2 <- p2 + geom_label(aes(525, 0.6,
+#'                          label = "~p[1]-p[2] == 0.05"), parse = TRUE,
+#'                          color = "black", size = 3)
+#'
+#' @export
+#'
+
+bayes_sim_betabin <- function(n1, n2, p1, p2, alpha_1, alpha_2, beta_1, beta_2, sig_level, alt, mc_iter = 5000){
+    count <- 0
+
+    set.seed(1)
+    if(is.null(p1) == TRUE & is.null(p2) == TRUE){
+      p1 <- rbeta(n=1, alpha_1, beta_1)
+      p2 <- rbeta(n=1, alpha_2, beta_2)
+    }else if(is.null(p1) == TRUE & is.null(p2) == FALSE){
+      p1 <- rbeta(n=1, alpha_1, beta_1)
+    }else if(is.null(p1) == FALSE & is.null(p2) == TRUE){
+      p2 <- rbeta(n=1, alpha_2, beta_2)
+    }
+
+    MC_samp <- function(n1 = n1, n2 = n2){
+      for(i in 1:mc_iter){
+
+        # x1 and x2 are observed frequency values, simulate by drawing from the binomial distribution
+        x1 <- rbinom(n=1, size=n1, prob=p1)
+        x2 <- rbinom(n=1, size=n2, prob=p2)
+
+        # estimating difference of proportions: p = p1 - p2
+        p_post_mean <- ((alpha_1 + x1) / (alpha_1 + beta_1 + n1)) - ((alpha_2 + x2) / (alpha_2 + beta_2 + n2))
+        p_post_var <- (((alpha_1 + x1) * (beta_1 + n1 - x1)) / ((alpha_1 + beta_1 + n1)^2 * (alpha_1 + beta_1 + n1 + 1))) +
+          (((alpha_2 + x2) * (beta_2 + n2 - x2)) / ((alpha_2 + beta_2 + n2)^2 * (alpha_2 + beta_2 + n2 + 1)))
+
+        # critical z-value of normal distribution
+        if(alt == "two.sided"){
+          z <- qnorm(1 - sig_level / 2)
+        }else if(alt == "greater" | "less"){
+          z <- qnorm(1 - sig_level)
+        }
+
+        # Condition to check if 0 falls in 100(1-sig_level)% Credible Interval
+        if(alt == "two.sided"){
+          # lower two-sided confidence bound
+          lb <- p_post_mean - z * (sqrt(p_post_var))
+          # upper two-sided confidence bound
+          ub <- p_post_mean + z * (sqrt(p_post_var))
+          if(0 < lb | 0 > ub){
+            count <- count + 1
+          }else{
+            count <- count
+          }
+        }else if(alt == "less"){ # Condition to check if 0 falls above UB of 100(1-sig_level)% Credible Interval
+          # lower two-sided confidence bound
+          lb <- -Inf
+          # upper two-sided confidence bound
+          ub <- p_post_mean + z * (sqrt(p_post_var))
+          if(0 > ub){
+            count <- count + 1
+          }else{
+            count <- count
+          }
+        }else if(alt == "greater"){
+          # lower two-sided confidence bound
+          lb <- p_post_mean - z * (sqrt(p_post_var))
+          # upper two-sided confidence bound
+          ub <- Inf
+          if(0 < lb){
+            count <- count + 1
+          }else{
+            count <- count
+          }
+        }
+      }
+      assurance <- count / mc_iter
+      return(assurance)
+    }
+
+    if(length(n1) > 1 & length(n2) > 1){
+      assurance_vals <- pbapply::pbmapply(MC_samp, n1 = n1, n2 = n2)
+      assur_tab <- as.data.frame(cbind(n1, n2, assurance_vals))
+      colnames(assur_tab) <- c("n1", "n2", "Assurance")
+
+      if(identical(n1, n2)){
+        assur_plot <- ggplot2::ggplot(assur_tab, alpha = 0.5, aes(x = .data$`n1`,
+                                                                  y = .data$Assurance)) +
+          ggplot2::geom_point(aes(x = .data$`n1`, y = .data$Assurance), lwd = 1.2) +
+          ggplot2::ggtitle("Assurance Values") +
+          ggplot2::xlab("Sample Size n = n1 = n2") + ggplot2::ylab("Assurance")
+        assur_plot <- structure(assur_plot, class = "ggplot")
+        return(list(assurance_table = assur_tab, assurance_plot = assur_plot, mc_samples = mc_iter))
+      }else{
+        return(list(assurance_table = assur_tab, mc_samples = mc_iter))
+      }
+    }
+
+
+    #return(list(assur_val = paste0("Assurance: ", round(assurance, 3)), mc_samples = mc_iter))
+
+}
+
+
+
+
+
